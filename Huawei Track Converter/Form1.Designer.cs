0231c96e30a362d80d73efea28389f4036119541
Aaron Schulz <aaron@race.co.nz>
2019-05-17 17:15:43 +1200

    Include form for basic UI

---
+++ Huawei Track Converter/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Huawei_Track_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.directoryEntry1 = new System.DirectoryServices.DirectoryEntry();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.listBoxFiles = new System.Windows.Forms.ListBox();
+            this.lblDistance = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "folder.png");
+            this.imageList1.Images.SetKeyName(1, "file.png");
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Choose Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemmory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(421, 68);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 2;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomEnabled = true;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(614, 330);
+            this.gMapControl1.TabIndex = 5;
+            this.gMapControl1.Zoom = 0D;
+            // 
+            // listBoxFiles
+            // 
+            this.listBoxFiles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBoxFiles.FormattingEnabled = true;
+            this.listBoxFiles.Location = new System.Drawing.Point(2, 42);
+            this.listBoxFiles.Name = "listBoxFiles";
+            this.listBoxFiles.Size = new System.Drawing.Size(413, 446);
+            this.listBoxFiles.TabIndex = 6;
+            this.listBoxFiles.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // lblDistance
+            // 
+            this.lblDistance.AutoSize = true;
+            this.lblDistance.Location = new System.Drawing.Point(421, 42);
+            this.lblDistance.Name = "lblDistance";
+            this.lblDistance.Size = new System.Drawing.Size(54, 13);
+            this.lblDistance.TabIndex = 7;
+            this.lblDistance.Text = "Distance:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1040, 535);
+            this.Controls.Add(this.lblDistance);
+            this.Controls.Add(this.listBoxFiles);
+            this.Controls.Add(this.gMapControl1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Huawei Track Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.DirectoryServices.DirectoryEntry directoryEntry1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button button1;
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+        private System.Windows.Forms.ListBox listBoxFiles;
+        private System.Windows.Forms.Label lblDistance;
+    }
+}
+
---
+++ Huawei Track Converter/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+
+namespace Huawei_Track_Converter
+{
+    public partial class Form1 : Form
+    {
+        private string folder = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //load up starting folder for development purposes
+            if (Debugger.IsAttached)
+            {
+                folder = @"C:\temp\examples";
+                LoadFolder(folder);
+            }
+
+            //basic map config
+            try
+            {
+                gMapControl1.MapProvider = GMapProviders.GoogleMap;
+                gMapControl1.Position = new PointLatLng(-43.1961334816182, 172);
+                gMapControl1.MinZoom = 0;
+                gMapControl1.MaxZoom = 24;
+                gMapControl1.Zoom = 9;
+            }
+            catch
+            { }
+        }
+
+        /// <summary>
+        /// Go get files for provided pathx`
+        /// </summary>
+        /// <param name="path"></param>
+        private void LoadFolder(string path)
+        {
+            try
+            {
+                listBoxFiles.Items.Clear();
+                listBoxFiles.BeginUpdate();
+
+                System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(path);
+
+                // For each file in the c:\ directory, create a ListViewItem
+                // and set the icon to the icon extracted from the file.
+                foreach (System.IO.FileInfo file in dir.GetFiles())
+                {
+                    listBoxFiles.Items.Add(file.Name);
+                }
+                listBoxFiles.EndUpdate();
+            }
+            catch
+            { }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderPicker = new FolderBrowserDialog();
+            if (folderPicker.ShowDialog() == DialogResult.OK)
+            {
+                folder = folderPicker.SelectedPath;
+                LoadFolder(folderPicker.SelectedPath);
+
+            }
+        }
+
+        private void LoadHuaweiDataOntoMap(HuaweiParser hp)
+        {
+            try
+            {
+                //create layer to store route
+                GMapRoute line_layer = new GMapRoute("single_line") { Stroke = new Pen(Brushes.Blue, 4) };
+                GMapOverlay line_overlay = new GMapOverlay("route");
+
+                line_overlay.Routes.Add(line_layer);
+                gMapControl1.Overlays.Add(line_overlay);
+
+                //simply add the points you want
+
+                foreach (HuaweiDatumPoint point in hp.Data)
+                {
+                    if (point.HasPosition)
+                    {
+                        line_layer.Points.Add(new PointLatLng(point.latitude, point.longitude));
+                    }
+                }
+                //line_layer.Points.Add(new PointLatLng(lat, lon));
+                //line_layer.Points.Add(new PointLatLng(lat2, lon2));
+
+                //Note that if you are using the MouseEventArgs you need to use local coordinates and convert them:
+                //line_layer.Points.Add(gMapControl1.FromLocalToLatLng(e.X, e.Y));
+
+                //To force the draw, you need to update the route
+                gMapControl1.UpdateRouteLocalPosition(line_layer);
+
+                //zoom to route
+                gMapControl1.ZoomAndCenterRoute(line_layer);
+                //you can even add markers at the end of the lines by adding markers to the same layer:
+                //GMapMarker marker_ = new GMarkerCross(p);
+                //line_overlay.Markers.Add(marker_);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listBoxFiles.SelectedItems.Count > 0)
+                {
+                    string path = $@"{folder}\{listBoxFiles.SelectedItems[0]}";
+                    HuaweiParser hp = new HuaweiParser(path);
+                    lblDistance.Text = "Distance: " + Convert.ToInt32(hp.TotalDistance).ToString() + "m";
+                    LoadHuaweiDataOntoMap(hp);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                MessageBox.Show(exception.Message);
+            }
+        }
+    }
+}
